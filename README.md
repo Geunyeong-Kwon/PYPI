b1ae0976c473ee9da60c1c70588014943296f7f1
Geunyeong-Kwon <geuny1472@naver.com>
2023-06-22 02:22:44 +0900

    commit changes

---
+++ README.md
@@ -0,0 +1,1 @@
+This is explain ML
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+  include_package_data=True,
+  name = 'gyk230621',
+  version = '0.0.1',
+  description = 'oss-ML',
+  author='gy0',
+  author_email = 'geuny1472@naver.com',
+  url = "https://github.com/Geunyeong-Kwon/PYPI",
+  download_url = "https://github.com/Geunyeong-Kwon/PYPI/archive/refs/tags/v0.01.zip",
+  istall_requires=['pytest'],
+  long_description = 'ML - math, numpy, pandas using',
+  long_description_content_type = 'text/markdpwn',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ]
+
+)
